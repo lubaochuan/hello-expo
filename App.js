84dc05616b44f334114593b96f422cc9da5846a7
lubaochuan <lubaochuan@gmail.com>
2017-10-06 16:13:16 -0500

    turned counter into its own component

--- App.js
+++ App.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Constants } from 'expo';
 
-export default class App extends Component {
+class CounterButton extends Component {
   state = {
     counter: 0,
   }
+
+  render(){
+    return (
+      <TouchableOpacity
+        style={{backgroundColor: 'white'}}
+        onPress={() => this.setState({counter: this.state.counter + 1})}>
+        <Text style={styles.paragraph}>
+          {this.state.counter}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
+}
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity
-          style={{backgroundColor: 'white'}}
-          onPress={() => this.setState({counter: this.state.counter + 1})}>
-          <Text style={styles.paragraph}>
-            {this.state.counter}
-          </Text>
-        </TouchableOpacity>
+        <CounterButton />
       </View>
     );
   }
