f375d7d3700aa80c438c04550ae4ca38ac7f4315
lubaochuan <lubaochuan@gmail.com>
2017-10-06 16:08:11 -0500

    added a counter

--- App.js
+++ App.js
@@ -3,12 +3,17 @@ import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Constants } from 'expo';
 
 export default class App extends Component {
+  state = {
+    counter: 0,
+  }
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={{backgroundColor: 'white'}}>
+        <TouchableOpacity
+          style={{backgroundColor: 'white'}}
+          onPress={() => this.setState({counter: this.state.counter + 1})}>
           <Text style={styles.paragraph}>
-            hello world!
+            {this.state.counter}
           </Text>
         </TouchableOpacity>
       </View>
